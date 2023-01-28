82960f951b21efe5650a425710bcfaae53146669
Lingli Feng <linglife@usc.edu>
2023-01-28 04:41:53 +0000

    fix error

--- hw1/test_ulliststr.cpp
+++ hw1/test_ulliststr.cpp
@@ -5,9 +5,83 @@
 
 #include "ulliststr.h"
 
+using namespace std;
 //Use this file to test your ulliststr implementation before running the test suite
+
+ULListStr * makeList(std::vector<std::string> const & values, bool pushBack)
+{
+	// circular list is not copyable so we have to pass it by pointer
+	ULListStr * list = new ULListStr();
+
+	if(pushBack)
+	{
+		for(std::vector<std::string>::const_iterator valuesIter = values.cbegin(); valuesIter != values.cend(); ++valuesIter)
+		{
+			list->push_back(*valuesIter);
+		}
+	}
+	else
+	{
+		// push values in reverse from the front
+		for(std::vector<std::string>::const_reverse_iterator valuesIter = values.crbegin(); valuesIter != values.crend(); ++valuesIter)
+		{
+			list->push_front(*valuesIter);
+			cout<<"The size of the list is : "<<list->size()<<endl;
+			cout<<"The front of the list is : "<<list->front()<<endl;
+		}
+	}
+
+	return list;
+}
+
+bool checkListContent(ULListStr *const list, std::vector<std::string> const &expectedValues)
+{
+	if(list->size() != expectedValues.size())
+	{
+		 cout<< "Size mismatch! Expected " << expectedValues.size() << " elements, got " << list->size() << " elements.";
+		 return false;
+	}
+
+	// aggregate all mismatches into one failure message
+	std::stringstream failureMessage;
+	failureMessage << "List content mismatch: ";
+	bool contentMismatch = false;
+
+	for(size_t index = 0; index < list->size(); ++index)
+	{
+		if(list->get(index) != expectedValues[index])
+		{
+			failureMessage << "Expected value at index " << index << " was " << expectedValues[index] << ", but element was actually " << list->get(index) << std::endl;
+			contentMismatch = true;
+		}
+	}
+
+	if(contentMismatch)
+	{
+		 cout<< failureMessage.str();
+		 return false;
+	}
+
+	return true;
+}
 
 int main(int argc, char* argv[])
 {
+		// first we create a vector to hold our data
+	std::vector<std::string> contents{"768", "1024", "1536"};
 
+	// then we create a new list with that data
+	ULListStr * populatedList = makeList(contents, false);
+
+		for(int i = 0;i<populatedList->size();i++)
+		{
+			cout<<populatedList->get(i)<<"  "<<endl;
+		}
+	// then we assert that the list contains that data
+	if(checkListContent(populatedList, contents))
+	cout<<"Test success"<<endl;
+	else
+	cout<<"Test failed"<<endl;
+
+	delete populatedList;
 }
--- hw1/ulliststr.cpp
+++ hw1/ulliststr.cpp
@@ -26,6 +26,128 @@ size_t ULListStr::size() const
 
 // WRITE YOUR CODE HERE
 
+void ULListStr::push_back(const std::string& val) {
+  if (empty())
+  {
+    head_ = new Item;
+    tail_ = new Item;
+    Item *temp = new Item;
+    temp->last++;
+    temp->val[0] = val;
+    temp->next = tail_;
+    temp->prev = head_;
+    head_->next = temp;
+    tail_->prev = temp;
+    size_++;
+    return;
+  }
+  if (tail_->prev->last < ARRSIZE)
+  {
+    tail_->prev->val[tail_->prev->last] = val;
+    tail_->prev->last++;
+    size_++;
+    return;
+  }
+  Item *temp = new Item;
+  temp->val[0] = val;
+  temp->last++;
+  tail_->prev->next = temp;
+  tail_->prev = temp;
+  size_++;
+}
+
+void ULListStr::pop_back() {
+  if (empty())
+  {
+    throw std::invalid_argument("Bad pop");
+  }
+  tail_->prev->last--;
+  if (tail_->prev->first == tail_->prev->last) {
+    Item *temp = tail_->prev;
+    tail_->prev = temp->prev;
+    delete temp;
+  }
+  if (head_->next == tail_) {
+    delete head_;
+    delete tail_;
+    head_ = tail_ = NULL;
+  }
+  size_--;
+}
+
+void ULListStr::push_front(const std::string& val) {
+  if (empty())
+  {
+    push_back(val);
+    return;
+  }
+  if (head_->next->first > 0)
+  {
+    head_->next->first--;
+    head_->next->val[head_->next->first] = val;
+    size_++;
+    return;
+  }
+  Item *temp = new Item;
+  temp->first = ARRSIZE - 1;
+  temp->last = ARRSIZE;
+  temp->val[temp->first] = val;
+  temp->next = head_->next;
+  head_->next = temp;
+  size_++;
+}
+
+void ULListStr::pop_front() {
+  if (empty())
+  {
+    throw std::invalid_argument("Bad pop");
+  }
+  head_->next->first++;
+  if (head_->next->first == head_->next->last) {
+    Item *temp = head_->next;
+    head_->next = temp->next;
+    delete temp;
+  }
+  if (head_->next == tail_) {
+    delete head_;
+    delete tail_;
+    head_ = tail_ = NULL;
+  }
+  size_--;
+}
+
+std::string const & ULListStr::back() const {
+  if (empty())
+  {
+    throw std::invalid_argument("Bad access");
+  }
+  return tail_->prev->val[tail_->prev->last-1];
+}
+
+std::string const & ULListStr::front() const {
+  if (empty())
+  {
+    throw std::invalid_argument("Bad access");
+  }
+  return head_->next->val[head_->next->first];
+}
+
+std::string* ULListStr::getValAtLoc(size_t loc) const 
+{
+ size_t i = 0;
+ Item *temp = head_->next;
+ while(temp != tail_){
+  for(size_t j = temp->first; j < temp->last; ++j)
+  {
+    if(i == loc) return &temp->val[j];
+    ++i;
+  }
+  temp = temp->next;
+ }
+ throw std::invalid_argument("Bad location");
+}
+
+
 void ULListStr::set(size_t loc, const std::string& val)
 {
   std::string* ptr = getValAtLoc(loc);
